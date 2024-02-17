051eae2f6fe35720e2e4df18344c309eae73e5b7
sagarapidev <106273002+sagarapidev@users.noreply.github.com>
2024-02-18 02:21:31 +0530

    #7 Refactor database configuration server side

---
+++ react_node_mssql/sja_app/server/config/dbConfig.js
@@ -0,0 +1,14 @@
+// dbconfig.js
+require('dotenv').config();
+
+const dbConfig = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,
+  database: process.env.DB_DATABASE,
+  options: {
+    trustServerCertificate: true
+  }
+};
+
+module.exports = dbConfig;
--- react_node_mssql/sja_app/server/server.js
+++ react_node_mssql/sja_app/server/server.js
@@ -1,38 +1,32 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const sql = require('mssql');
-const customCorsMiddleware = require('./middleware/customCorsMiddleware'); // Import the custom CORS middleware
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const sql = require("mssql");
+const customCorsMiddleware = require("./middleware/customCorsMiddleware");
+const dbConfig = require("./config/dbConfig"); // Import the SQL Server configuration
+require("dotenv").config();
 
 const app = express();
-const NODE_APP_PORT = process.env.SERVER_PORT || 5001; // Fallback to 5001 if SERVER_PORT is not defined
+const NODE_APP_PORT = process.env.SERVER_PORT || 5001;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(customCorsMiddleware); // Use the custom CORS middleware here
-
-// SQL Server configuration
-const config = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  server: process.env.DB_SERVER,
-  database: process.env.DB_DATABASE,
-  options: {
-    trustServerCertificate: true
-  }
-};
+app.use(customCorsMiddleware);
 
 // API to fetch users data
-app.get('/users', async (req, res) => {
+app.get("/users", async (req, res) => {
   try {
-    const pool = await sql.connect(config);
-    const result = await pool.request().query('SELECT * FROM users');
+    const pool = await sql.connect(dbConfig); // Use dbConfig for SQL Server configuration
+    const result = await pool.request().query("SELECT * FROM users");
     res.json(result.recordset);
   } catch (err) {
-    console.error('SQL error', err);
-    res.status(500).json({ error: 'Internal Server Error', message: err.message });
+    console.error("SQL error", err);
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", message: err.message });
   }
 });
 
 // Start server
-app.listen(NODE_APP_PORT, () => console.log(`Node server running on ${NODE_APP_PORT}`));
+app.listen(NODE_APP_PORT, () =>
+  console.log(`Node server running on ${NODE_APP_PORT}`)
+);
