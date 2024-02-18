c1c558b29287576cf0b7a5ea2e9776dc6518480f
sagarapidev <106273002+sagarapidev@users.noreply.github.com>
2024-02-18 07:29:42 +0530

    #12 Create app header component

--- react_node_mssql/sja_app/src/App.js
+++ react_node_mssql/sja_app/src/App.js
@@ -1,8 +1,14 @@
+import React from 'react';
+import Layout from "antd/lib/layout/layout";
+import AppHeader from "./component/Header/AppHeader";
+
 function App() {
-  return (
-    <div className="App">
-      <h1>ReactJs +Node + SQLSERVER</h1>
-    </div>
-  );
+  return <div className="App">
+    <Layout>
+      <AppHeader/>
+      <AppHeader/>
+      <AppHeader/>
+    </Layout>
+  </div>;
 }
 export default App;
---
+++ react_node_mssql/sja_app/src/component/Header/AppHeader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { PageHeader } from "antd";
+const AppHeader = () => {
+  return (
+    <PageHeader
+      className="site-page-header"
+      title="Adya"
+      subTitle="Born to explore the world"
+    />
+  );
+};
+
+export default AppHeader;
